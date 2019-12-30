3fb4742a1eaa16b5e9eb7224aa518f01240edfa4
Victor <vachavarro@gmail.com>
2019-12-29 23:25:21 -0500

    feat(develop): base folder/file structure

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import Counter from './components/counter/Counter';
+
+render(<Counter />, document.getElementById('main'));
---
+++ src/components/counter/Counter.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+export default class Counter extends React.Component {
+  public state = {
+    count: 0,
+  };
+
+  public increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  public decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  public render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
